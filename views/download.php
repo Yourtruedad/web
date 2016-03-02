cadeba697a24a7f77281511050ed36b3127c2bf5
YourTrueDad <p.przeworowski@gmail.com>
2016-03-02 22:37:20 +0100

    download page

--- views/download.php
+++ views/download.php
@@ -1,17 +1,13 @@
 <h1>Download</h1>
 <h3>Game Client</h3>
 <ul>
-	<li><a href="#" title="Get it now!">Download the game from Rapidshare</a></li>
-	<li>Download the game from Megaupload</li>
-	<li>Download the game from Hotfiles</li>
+	<li>Available soon</li>
 </ul>
-<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+<p class="lead">The client contains everything you need to join the game.</p>
 <hr>
 <h3>Game Client Patch</h3>
 <ul>
-	<li><a href="#" title="Get it now!">Download the patch from Rapidshare</a></li>
-	<li>Download the patch from Megaupload</li>
-	<li>Download the patch from Hotfiles</li>
+	<li>Available soon</li>
 </ul>
-<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+<p class="lead">The patch is not mandatory is you have downloaded the client from the link above.</p>
 <div class="bg-primary info-box box-border">Remember that you can always reach out to us on our <a href="<?=FORUM_LINK?>" class="link-white-bold" title="Forum">message board</a> if you have any issues with the game.</div>
