63d2b6b88d8304888f87d1ba0a4f16e5a65a77fd
YourTrueDad <p.przeworowski@gmail.com>
2016-03-06 19:23:04 +0100

    game client and patch links

--- views/download.php
+++ views/download.php
@@ -1,13 +1,17 @@
 <h1>Download</h1>
 <h3>Game Client</h3>
 <ul>
-	<li>Available soon</li>
+    <li><a href="http://www.mediafire.com/download/cidoicp46wdbyt8/EverWinterMu.exe" target="_blank">MediaFire (Mirror #1)</a></li>
+    <li><a href="https://mega.nz/#!ilsTTY6C!RIH-enzC5D9010gNypny0EFJroVywtNGl3kmVpOZ-UU" target="_blank">Mega (Mirror #2)</a></li>
+    <li><a href="http://everwintermu.com/EverWinterMu.exe" target="_blank">EverWinter Server (Mirror #3)</a></li>
 </ul>
 <p class="lead">The client contains everything you need to join the game.</p>
 <hr>
 <h3>Game Client Patch</h3>
 <ul>
-	<li>Available soon</li>
+    <li><a href="http://www.mediafire.com/download/6mn4pbeozp0mb34/patch.exe" target="_blank">MediaFire (Mirror #1)</a></li>
+    <li><a href="https://mega.nz/#!Ws1kWISI!g-toIan8sQgBKbHsb0ETdT-npg4BcBw2l-wq-7RK3aA" target="_blank">Mega (Mirror #2)</a></li>
+    <li><a href="http://everwintermu.com/patch.exe" target="_blank">EverWinter Server (Mirror #3)</a></li>
 </ul>
 <p class="lead">The patch is not mandatory if you downloaded the client from the link above.</p>
 <div class="bg-primary info-box box-border">Remember that you can always reach out to us on our <a href="<?=FORUM_LINK?>" class="link-white-bold" title="Forum">message board</a> if you have any issues with the game.</div>
