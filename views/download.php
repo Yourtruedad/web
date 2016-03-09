707e383c5db0fa5bcde74208a9793749dc4efbaa
YourTrueDad <p.przeworowski@gmail.com>
2016-03-09 20:05:18 +0100

    download new mega links

--- views/download.php
+++ views/download.php
@@ -2,7 +2,7 @@
 <h3>Game Client</h3>
 <ul>
     <li><a href="http://www.mediafire.com/download/cidoicp46wdbyt8/EverWinterMu.exe" target="_blank">MediaFire (Mirror #1)</a></li>
-    <li><a href="https://mega.nz/#!ilsTTY6C!RIH-enzC5D9010gNypny0EFJroVywtNGl3kmVpOZ-UU" target="_blank">Mega (Mirror #2)</a></li>
+    <li><a href="https://mega.nz/#!2xdDTCQR!IfZdDLg-IN7D3WNJawmtkHjzY0qDQ7Ynh8iOLVLZNdc" target="_blank">Mega (Mirror #2)</a></li>
     <li><a href="http://everwintermu.com/EverWinterMu.exe" target="_blank">EverWinter Server (Mirror #3)</a></li>
 </ul>
 <p class="lead">The client contains everything you need to join the game.</p>
@@ -10,7 +10,7 @@
 <h3>Game Client Patch</h3>
 <ul>
     <li><a href="http://www.mediafire.com/download/6mn4pbeozp0mb34/patch.exe" target="_blank">MediaFire (Mirror #1)</a></li>
-    <li><a href="https://mega.nz/#!Ws1kWISI!g-toIan8sQgBKbHsb0ETdT-npg4BcBw2l-wq-7RK3aA" target="_blank">Mega (Mirror #2)</a></li>
+    <li><a href="https://mega.nz/#!W0NjhBTR!BDjog4Q9UpiTF50r_AXZONrPn7AFiM2Gpp8U6ANSWyc" target="_blank">Mega (Mirror #2)</a></li>
     <li><a href="http://everwintermu.com/patch.exe" target="_blank">EverWinter Server (Mirror #3)</a></li>
 </ul>
 <p class="lead">The patch is not mandatory if you downloaded the client from the link above.</p>
