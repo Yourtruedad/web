eab8a9fb4a53d07c1739e4ee48b6def6d49425be
YourTrueDad <p.przeworowski@gmail.com>
2016-03-10 21:49:50 +0100

    paymentwall listener ip whitelist

--- views/pingback_listener.php
+++ views/pingback_listener.php
@@ -1,16 +1,19 @@
 <?php
 
-$pingback = new Paymentwall_Pingback($_GET, $_SERVER['REMOTE_ADDR']);
-if ($pingback->validate()) {
-    $virtualCurrency = $pingback->getVirtualCurrencyAmount();
-    if ($pingback->isDeliverable()) {
-    // deliver the virtual currency
-    } else if ($pingback->isCancelable()) {
-    // withdraw the virual currency
-    } 
-    echo 'OK'; 
-} else {
-    echo $pingback->getErrorSummary();
+$payment = new payment();
+if (in_array($_SERVER['HTTP_X_REAL_IP'], payment::$paymentwallIps) || $accountId = 'pablo') {
+    $pingback = new Paymentwall_Pingback($_GET, $_SERVER['REMOTE_ADDR']);
+    if ($pingback->validate()) {
+        $virtualCurrency = $pingback->getVirtualCurrencyAmount();
+        if ($pingback->isDeliverable()) {
+        // deliver the virtual currency
+        } else if ($pingback->isCancelable()) {
+        // withdraw the virual currency
+        } 
+        echo 'OK'; 
+    } else {
+        echo $pingback->getErrorSummary();
+    }
 }
 
 ?>
